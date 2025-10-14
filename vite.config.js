c74e4f6202765e81a3fd14b595726fe3432b672b
Netlopeds <mrproatmcorentiixm@gmail.com>
2025-10-15 03:44:39 +0800

    Fix base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/tenth-life-appdev/',
+  base: '/tenth-life-vue/',
   publicDir: 'public',
   resolve: {
     alias: {
